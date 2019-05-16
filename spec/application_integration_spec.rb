359d0f10bb21629f50b37529da312eaf7e84084c
Leslie Osorio <lesosorio17@gmail.com>
2019-05-16 20:03:01 +0000

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -17,12 +17,12 @@ describe "GET '/' - Greeting Form" do
 end
 
 describe "POST '/greet' - User Greeting" do
-  it 'greets the user personally based on their user_name in the form' do
-    visit '/'
+    it 'greets the user personally based on their user_name in the form' do
+      visit '/'
 
-    fill_in(:user_name, :with => "Avi")
-    click_button "Submit"
+      fill_in(:user_name, :with => "Avi")
+      click_button "Submit"
 
-    expect(page).to have_text("Hi Avi, nice to meet you!")
+      expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
